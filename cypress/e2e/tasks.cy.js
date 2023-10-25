ed55da810f6887ec176651f3e77ccd9675789dbc
Antonio Marques <antonio.marquess@outlook.com.br>
2023-10-25 17:31:39 -0300

    Dados dinâmicos com Faker

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,10 +1,12 @@
 /// <reference types="cypress" />
 
+import {faker} from '@faker-js/faker' 
+
 describe('Tasks', () => {
   it('Deve cadastar uma nova tarefa', () => {
     cy.visit('http://localhost:8080')
     cy.get('input[placeholder="Add a new Task"]')
-      .type('Ler o syllabus para tira certificação em teste de software')
+      .type(faker.music.songName())
     cy.contains('button', 'Create').click()
 
   })
