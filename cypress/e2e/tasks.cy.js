4a6c92bab4714aa25ee015967eeb3e8cb1b04b7b
Antonio Marques <antonio.marquess@outlook.com.br>
2023-10-25 13:28:33 -0300

    Campos de Texto

---
+++ cypress/e2e/tasks.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe('Tasks', () => {
+  it('Deve cadastar uma nova tarefa', () => {
+    cy.visit('http://localhost:8080')
+    cy.get('input[placeholder="Add a new Task"]')
+      .type('Ler o syllabus para tira uma certificação em teste de software')
+
+  })
+})
