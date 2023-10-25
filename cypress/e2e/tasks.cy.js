ecc80e2157713ed0169712896d9d26e7db515af9
Antonio Marques <antonio.marquess@outlook.com.br>
2023-10-25 17:58:09 -0300

    Consumindo a API Rest

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,12 +1,20 @@
 /// <reference types="cypress" />
 
-import {faker} from '@faker-js/faker' 
-
 describe('Tasks', () => {
   it('Deve cadastar uma nova tarefa', () => {
+
+    cy.request({
+      url: 'http://localhost:3333/helper/tasks',
+      method: 'DELETE',
+      body: { name: "Ler o syllabus para tira certificação em teste de software",
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(204)
+    })
+
     cy.visit('http://localhost:8080')
     cy.get('input[placeholder="Add a new Task"]')
-      .type(faker.music.songName())
+      .type('Ler o syllabus para tira certificação em teste de software')
     cy.contains('button', 'Create').click()
 
   })
