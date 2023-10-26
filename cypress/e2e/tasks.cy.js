540b23ddc01b923f14c0fa8dcac66d06cf1a426a
Antonio Marques <antonio.marquess@outlook.com.br>
2023-10-26 00:09:59 -0300

    Organização por contexto

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,40 +1,43 @@
 /// <reference types='cypress' />
 
 describe('Tasks', () => {
-  it('Deve cadastar uma nova tarefa', () => {
+  context('Cadastro de tasks', () => {
+    it('Deve cadastar uma nova tarefa', () => {
 
-    const taskName = 'Ler o syllabus para tira certificação em teste de software';
+      const taskName = 'Ler o syllabus para tira certificação em teste de software';
 
-    cy.deleteTaskByName(taskName)
-    cy.createTask(taskName);
+      cy.deleteTaskByName(taskName)
+      cy.createTask(taskName);
 
-    cy.contains('main div p', 'Ler o syllabus para tira certificação em teste de software')
-      .should('be.visible')
-      .should('have.text', taskName)
+      cy.contains('main div p', 'Ler o syllabus para tira certificação em teste de software')
+        .should('be.visible')
+        .should('have.text', taskName)
+    });
+
+    it('Não deve cadastar uma nova tarefa duplicada', () => {
+
+      const task = {
+        name: 'Fazer compras',
+        is_done: false,
+      }
+
+      cy.deleteTaskByName(task.name)
+      cy.postTask(task);
+
+      cy.createTask(task.name);
+
+      cy.get('.swal2-html-container')
+        .should('be.visible')
+        .should('have.text', 'Task already exists!');
+
+      cy.get('.swal2-confirm')
+        .click();
+    });
+
+    it('Campo obrigatorio', () => {
+      cy.createTask();
+      cy.isRequired('This is a required field')
+    })
   });
 
-  it('Não deve cadastar uma nova tarefa duplicada', () => {
-
-    const task = {
-      name: 'Fazer compras',
-      is_done: false,
-    }
-
-    cy.deleteTaskByName(task.name)
-    cy.postTask(task);
-
-    cy.createTask(task.name);
-
-    cy.get('.swal2-html-container')
-      .should('be.visible')
-      .should('have.text', 'Task already exists!');
-
-    cy.get('.swal2-confirm')
-      .click();
-  });
-
-  it('Campo obrigatorio', () => {
-    cy.createTask();
-    cy.isRequired('This is a required field')
-  })
 });
