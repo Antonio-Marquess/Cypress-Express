6361644ede44bf297571fd6147ed4ee4744a9366
Antonio Marques <antonio.marquess@outlook.com.br>
2023-10-25 21:04:00 -0300

    Testes independentes

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -13,12 +13,52 @@ describe('Tasks', () => {
     });
 
     cy.visit('http://localhost:8080');
-    cy.get('input[placeholder="Add a new Task"]').type(
-      'Ler o syllabus para tira certificação em teste de software'
-    );
-    cy.contains('button', 'Create').click();
+
+    cy.get('input[placeholder="Add a new Task"]')
+      .type('Ler o syllabus para tira certificação em teste de software');
+
+    cy.contains('button', 'Create')
+      .click();
+
     cy.contains('main div p', 'Ler o syllabus para tira certificação em teste de software')
       .should('be.visible')
       .should('have.text', 'Ler o syllabus para tira certificação em teste de software')
+  });
+
+  it('Não deve cadastar uma nova tarefa duplicada', () => {
+    cy.request({
+      url: 'http://localhost:3333/helper/tasks',
+      method: 'DELETE',
+      body: {
+        name: 'Fazer compras',
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(204);
+    });
+
+    cy.request({
+      url: 'http://localhost:3333/tasks',
+      method: 'POST',
+      body: {
+        name: 'Fazer compras', is_done: false,
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(201);
+    });
+
+    cy.visit('http://localhost:8080');
+
+    cy.get('input[placeholder="Add a new Task"]')
+      .type('Fazer compras');
+
+    cy.contains('button', 'Create')
+      .click();
+
+    cy.get('.swal2-html-container')
+      .should('be.visible')
+      .should('have.text', 'Task already exists!');
+      
+    cy.get('.swal2-confirm')
+      .click();
   });
 });
