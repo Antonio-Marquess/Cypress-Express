887387d35aa06abd02e98a0bda0102ccc14cd4ee
Antonio Marques <antonio.marquess@outlook.com.br>
2023-10-25 21:15:21 -0300

    Variáveis e Constantes

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -2,11 +2,14 @@
 
 describe('Tasks', () => {
   it('Deve cadastar uma nova tarefa', () => {
+
+    const taskName = 'Ler o syllabus para tira certificação em teste de software';
+
     cy.request({
       url: 'http://localhost:3333/helper/tasks',
       method: 'DELETE',
       body: {
-        name: 'Ler o syllabus para tira certificação em teste de software',
+        name: taskName,
       },
     }).then((response) => {
       expect(response.status).to.eq(204);
@@ -15,23 +18,26 @@ describe('Tasks', () => {
     cy.visit('http://localhost:8080');
 
     cy.get('input[placeholder="Add a new Task"]')
-      .type('Ler o syllabus para tira certificação em teste de software');
+      .type(taskName);
 
     cy.contains('button', 'Create')
       .click();
 
     cy.contains('main div p', 'Ler o syllabus para tira certificação em teste de software')
       .should('be.visible')
-      .should('have.text', 'Ler o syllabus para tira certificação em teste de software')
+      .should('have.text', taskName)
   });
 
   it('Não deve cadastar uma nova tarefa duplicada', () => {
+
+    const task = {
+      name: 'Fazer compras',
+      is_done: false,
+    }
     cy.request({
       url: 'http://localhost:3333/helper/tasks',
       method: 'DELETE',
-      body: {
-        name: 'Fazer compras',
-      },
+      body: { name: task.name },
     }).then((response) => {
       expect(response.status).to.eq(204);
     });
@@ -39,9 +45,7 @@ describe('Tasks', () => {
     cy.request({
       url: 'http://localhost:3333/tasks',
       method: 'POST',
-      body: {
-        name: 'Fazer compras', is_done: false,
-      },
+      body: task,
     }).then((response) => {
       expect(response.status).to.eq(201);
     });
@@ -49,7 +53,7 @@ describe('Tasks', () => {
     cy.visit('http://localhost:8080');
 
     cy.get('input[placeholder="Add a new Task"]')
-      .type('Fazer compras');
+      .type(task.name);
 
     cy.contains('button', 'Create')
       .click();
@@ -57,7 +61,7 @@ describe('Tasks', () => {
     cy.get('.swal2-html-container')
       .should('be.visible')
       .should('have.text', 'Task already exists!');
-      
+
     cy.get('.swal2-confirm')
       .click();
   });
