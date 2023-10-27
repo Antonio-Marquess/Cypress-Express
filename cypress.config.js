7cfc946914e91a1ca39ac1a196eb49acba962f7e
Antonio Marques <antonio.marquess@outlook.com.br>
2023-10-26 22:24:46 -0300

    Viewport Size, cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,13 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:8080",
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    
     env: {
       apiUrl: "http://localhost:3333",
     },
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
